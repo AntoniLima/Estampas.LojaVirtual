c86f2cdd529e866e98bf10b5c199d46cd109e68f
Antonio Eduardo de Oliveira Lima <devilre27@gmail.com>
2017-05-23 06:32:16 -0300

    Insersão do Bootstrap

---
+++ Estampas.LojaVirtual.Web/Models/ProdutosViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Estampas.LojaVirtual.Web.Models
+{
+    public class ProdutosViewModel
+    {
+    }
+}
